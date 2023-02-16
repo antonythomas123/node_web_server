e1f0f43f30ea529fd7e2d3691fc280d837e8919e
antonythomas123 <antonythomas993@gmail.com>
2023-02-16 15:52:22 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const logEvents = require('./log');
+
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 3500;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
