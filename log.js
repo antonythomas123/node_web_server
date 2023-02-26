16a1fee8f297daea077247c138f27e798cf4f769
antonythomas123 <antonythomas993@gmail.com>
2023-02-26 10:46:49 +0530

    Files added, Minimal Server setup and contentType added

--- log.js
+++ log.js
@@ -1,11 +1,11 @@
-const {format} = require('date-fns')
-const {v4: uuid} = require('uuid');
+const { format } = require('date-fns')
+const { v4: uuid } = require('uuid');
 
 const fs = require('fs');
 const path = require('path');
 const fsPromises = require('fs').promises;
 
-const logEvents = async(message) => {
+const logEvents = async (message, logName) => {
     const logTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
     const logItem = `${logTime}\t${uuid()}\t${message}`;
 
@@ -13,13 +13,15 @@ const logEvents = async(message) => {
         if (!fs.existsSync(path.join(__dirname, 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, 'logs'));
         }
-        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem, (err) => {
+        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem, (err) => {
             if (err) {
                 console.error(err);
             }
         })
-    
+
     } catch (err) {
         console.error(err)
     }
 }
+
+module.exports = logEvents;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const logEvents = require('./log');
+const EventEmitter = require('events');
 
 const fs = require('fs');
 const path = require('path');
@@ -6,15 +7,53 @@ const fsPromises = require('fs').promises;
 
 const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT || 3500;
+class Emitter extends EventEmitter { };
+const myEmitter = new Emitter();        //initialize Object
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
-});
+const PORT = process.env.PORT || 3500;  //port for our web server
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+//minimal server
+const server = http.createServer((req,res) => {
+    console.log(req.url, req.method);
+
+    //extension of req url
+    const extension = path.extname(req.url);
+
+    //setting up the contentType
+    let contentType;
+
+    switch(extension){
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType='text/javascript';
+            break;
+        case '.json': 
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default: 
+            contentType = 'text/html';
+            break;
+    }
+})
+
+
+
+
+
+// myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+
+//server listening for request
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
