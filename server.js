44a3f90ae76c6b1ee28b690c514d75a1042485df
antonythomas123 <antonythomas993@gmail.com>
2023-02-26 11:19:52 +0530

    serveFile, 404, fileExist check,dealing with JSON & Image added

--- server.js
+++ server.js
@@ -12,6 +12,28 @@ const myEmitter = new Emitter();        //initialize Object
 
 const PORT = process.env.PORT || 3500;  //port for our web server
 
+//basic serveFile FUnction : function that can be called for a 404 or fileExist
+const serveFile = async (filePath,contentType,response) => {
+    try{
+        //getting data from the file
+        const rawData =  await fsPromises.readFile(
+            filePath, 
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        //to deal with json files
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(200, {'Content-Type': contentType});
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    }catch(err){
+        console.log(err);
+        response.statusCode = 500;  //server error
+        response.end();
+    }
+}
+
 //minimal server
 const server = http.createServer((req,res) => {
     console.log(req.url, req.method);
@@ -44,6 +66,43 @@ const server = http.createServer((req,res) => {
         default: 
             contentType = 'text/html';
             break;
+    }
+
+    //Setting the filePath
+    let filePath = 
+        contentType === 'text/html' && req.url === '/'
+            ? path.join(__dirname, 'views', 'index.html')
+            : contentType === 'text/html' && req.url.slice(-1) === '/'
+                ? path.join(__dirname, 'views', req.url /* url of the subdirectory*/, 'index.html')
+                : contentType === 'text/html'
+                    ? path.join(__dirname, 'views', req.url)
+                    : path.join(__dirname, req.url);
+
+    //makes the .html extension not required in the browser
+    if(!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    //To check whether the requested file path exists
+    const fileExists = fs.existsSync(filePath);
+
+    if(fileExists){
+        //serve the file
+        serveFile(filePath, contentType, res);
+    }else{
+        //404 || 301
+        //console.log(path.parse(filePath));
+        switch(path.parse(filePath).base){
+            case 'old-page.html':
+                res.writeHead(301, {'Location': '/new-page.html'});
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, {'Location': '/'});
+                res.end();
+                break;
+            default:
+                //serve a 404 response
+                serveFile(path.join(__dirname, 'views', '404.html'), 'text/html', res);
+        }
     }
 })
 
