81d79c207a78b338200fa58f67af6c9ae2a13690
antonythomas123 <antonythomas993@gmail.com>
2023-03-06 11:34:37 +0530

    app.use vs app.all & Improved 404 error handler

--- server.js
+++ server.js
@@ -70,8 +70,17 @@ app.get('/old-page.html', (req, res) => {
     res.redirect(301, '/new-page.html'); //302 by default
 })
 
-app.get('/*', (req, res) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+app.all('*', (req, res) => {
+    res.status(404);
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    }
+    else if(req.accepts('json')){
+        res.json({error: "404 Not Found"});
+    }else{
+        res.type('txt').send("404 Not Found");
+    }
+    
 })
 
 //Route Handlers
