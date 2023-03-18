580347a3153d4ab70b7042fc29d22216e6176133
antonythomas123 <antonythomas993@gmail.com>
2023-03-18 18:30:09 +0530

    config file added and Updated employee Controller

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 
 //third party middleware
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 
 //const logEvents = require('./middleware/log');
 const { logger, logEvents } = require('./middleware/log');
@@ -13,29 +14,9 @@ const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;  //port for our web server
 
 //custom middleware's
-
-// app.use((req, res, next) => {
-//     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`,'reqLogs.txt')
-//     console.log(`${req.method} ${req.path}`);
-//     next();
-// })
-
-//                  OR
-
 app.use(logger);
 
 //third party middleware
-const whitelist=['https://www.yoursite.com','http://127.0.0.1:5500','http://localhost:3500']; //whitelist that the backend will not show cors
-const corsOptions = {
-    origin: (origin, callback) => {
-        if(whitelist.indexOf(origin)!== -1 || !origin){
-            callback(null,true)
-        }else{
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
 //built-in middleware's
@@ -48,34 +29,10 @@ app.use(express.json());
 
 //built-in middleware for static files
 app.use('/',express.static(path.join(__dirname, '/public')));
-app.use('/subdir',express.static(path.join(__dirname, '/public')));
 
 //express route for static file
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
-
-//Express route
-// app.get('/', (req, res) => {
-//     //res.sendFile('./views/index.html', {root: __dirname});
-//     res.sendFile(path.join(__dirname, 'views', 'index.html'));
-// })
-//OR
-//using regex in routes
-
-/*app.get('^/$|/index(.html)?', (req, res) => {
-    //res.sendFile('./views/index.html', {root: __dirname});
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-})*/
-
-
-// app.get('/new-page(.html)?', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-// })
-
-// app.get('/old-page.html', (req, res) => {
-//     res.redirect(301, '/new-page.html'); //302 by default
-// })
 
 app.all('*', (req, res) => {
     res.status(404);
@@ -89,30 +46,6 @@ app.all('*', (req, res) => {
     }
     
 })
-
-//Route Handlers
-
-// app.get('/hello(.html)?', (req, res, next) => {
-//     console.log('Attempeted to load hello.html');
-//     next()
-// }, (req, res) => {
-//     res.send('Hello World');
-// })
-
-// //Chaining route handlers
-// const one = (req, res, next) => {
-//     console.log('One');
-//     next();
-// }
-// const two = (req, res, next) => {
-//     console.log('two');
-//     next();
-// }
-// const three = (req, res) => {
-//     console.log('Three');
-//     res.send('Finished');
-// }
-// app.get('/chain.html', [one, two, three]);
 
 app.use(errorHandler);
 
